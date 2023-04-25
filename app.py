bb65d6da194772aab0f25cf0c13457967d8f8bae
11136011ntub <130752709+11136011ntub@users.noreply.github.com>
2023-04-25 14:48:08 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -397,8 +397,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, flex_message)
     elif re.match('日式美食',message):
         flex_message = FlexSendMessage(
-            alt_text='行銷搬進大程式',
-            contents={...} #json貼在這裡
+            line_bot_api.reply_message(event.reply_token, TextSendMessage('歐伊西'))
         )
         line_bot_api.reply_message(event.reply_token, flex_message)
     elif re.match('韓式美食',message):
@@ -670,8 +669,7 @@ flex_message = FlexSendMessage(
         line_bot_api.reply_message(event.reply_token, flex_message)
     elif re.match('美式美食',message):
         flex_message = FlexSendMessage(
-            alt_text='行銷搬進大程式',
-            contents={...} #json貼在這裡
+            line_bot_api.reply_message(event.reply_token, TextSendMessage('delicious'))
         )
         line_bot_api.reply_message(event.reply_token, flex_message)
     else:
